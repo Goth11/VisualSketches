d51cb5d83599790769a4beb0872ce4e3dd37c36a
Goth11 <42772160+Goth11@users.noreply.github.com>
2021-12-16 22:11:34 +0200

    Create sketch.js

---
+++ Interconnection/sketch.js
@@ -0,0 +1,40 @@
+let arr = [], maxR;
+
+function setup() {
+  createCanvas(800, 800);
+  maxR = Math.sqrt(width * width + height * height) / 2;
+  for (let i = 0; i < min(width, height) / 5; i++) {
+    let ok = false;
+    while (!ok) {
+      ok = true;
+      let vertices = [];
+      for (let j = 0; j < 3; j++) {
+        let r = random(maxR);
+        let a = random(TWO_PI);
+        vertices.push(new Vertex(r, a));
+      }
+
+      if (!Particle.isTriangle(vertices)) {
+          ok = false;
+      }
+      else {
+        let scl = 1;
+        let rSpeed = random(-scl, scl);
+        let scl2 = 0.0025;
+        let aSpeed = random(-scl2, scl2);
+        arr.push(new Particle(vertices, rSpeed, aSpeed))
+      }
+    }
+  }
+  background(0);return;
+}
+
+function draw() {
+  //background(0);
+  translate(width/2,height/2);
+  for (let ind = 0; ind < 10; ind++)
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].draw();
+    arr[i].update();
+  }
+}
